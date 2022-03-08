301f37bdcce1f773c1ca885d64be4c982d0977c2
eldridge.torrefranca <eldridge.torrefranca@lingarogroup.com>
2022-03-08 20:22:41 +0800

    First commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+
+export class UsersService {
+  getUsers(): any {
+    return [
+      {
+        id: 1,
+        firstName: 'Oliver',
+        lastName: 'Gartland',
+      },
+      {
+        id: 2,
+        firstName: 'Anna',
+        lastName: 'Watts',
+      },
+      {
+        id: 3,
+        firstName: 'Andy',
+        lastName: 'Brown',
+      },
+      {
+        id: 4,
+        firstName: 'Maisie',
+        lastName: 'Curtis',
+      },
+      {
+        id: 5,
+        firstName: 'Jonathan',
+        lastName: 'Curtis',
+      },
+      {
+        id: 6,
+        firstName: 'Jonathan',
+        lastName: 'Wright',
+      },
+      {
+        id: 7,
+        firstName: 'Jennifer',
+        lastName: 'Tomkinson',
+      },
+      {
+        id: 8,
+        firstName: 'Rich',
+        lastName: 'Richman',
+      },
+    ];
+  }
+}
