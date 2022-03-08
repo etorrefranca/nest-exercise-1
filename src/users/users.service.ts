ae2989a17332243daf2855e2836daf006cd5ec2c
eldridge.torrefranca <eldridge.torrefranca@lingarogroup.com>
2022-03-08 21:06:45 +0800

    Enhanced Users Endpoint, Code typing, & Sorting

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,48 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { UserData } from './users.interface';
 
+@Injectable()
 export class UsersService {
-  getUsers(): any {
+  getUserByAlphabet(): UserData[] {
+    const users = this.getUsers();
+
+    return users.sort((firstUser: UserData, secondUser: UserData) => {
+      const firstUserName = firstUser.firstName;
+      const secondUserName = secondUser.firstName;
+
+      if (firstUserName === secondUserName) {
+        if (firstUser.lastName < secondUser.lastName) return -1;
+        if (firstUser.lastName > secondUser.lastName) return 1;
+        return 0;
+      }
+
+      if (firstUserName < secondUserName) return -1;
+      if (firstUserName > secondUserName) return 1;
+      return 0;
+    });
+  }
+
+  getUsers(): UserData[] {
     return [
       {
         id: 1,
         firstName: 'Oliver',
-        lastName: 'Gartland',
+        lastName: 'Gartland'
       },
       {
         id: 2,
         firstName: 'Anna',
-        lastName: 'Watts',
+        lastName: 'Watts'
       },
       {
         id: 3,
         firstName: 'Andy',
-        lastName: 'Brown',
+        lastName: 'Brown'
       },
       {
         id: 4,
         firstName: 'Maisie',
-        lastName: 'Curtis',
+        lastName: 'Curtis'
       },
       {
         id: 5,
         firstName: 'Jonathan',
-        lastName: 'Curtis',
+        lastName: 'Curtis'
       },
       {
         id: 6,
         firstName: 'Jonathan',
-        lastName: 'Wright',
+        lastName: 'Wright'
       },
       {
         id: 7,
         firstName: 'Jennifer',
-        lastName: 'Tomkinson',
+        lastName: 'Tomkinson'
       },
       {
         id: 8,
         firstName: 'Rich',
-        lastName: 'Richman',
-      },
+        lastName: 'Richman'
+      }
     ];
   }
 }
